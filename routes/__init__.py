0726c95b30770151de07a29b3b4a822e6a193b4e
Hector G <hmgarcia06@gmail.com>
2023-10-15 20:48:20 -0400

    Testing

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,10 @@
 from flask import jsonify
-
+from config import FIREBASE_CREDENTIALS
 allowed_origins = ['https://irs-copilot.vercel.app/', 'https://irs-copilot.vercel.app', 'http://localhost:3000']
 
 def register(app):
     @app.route('/')
     def health():
-        return jsonify({'status': 'ok'})
+        return jsonify({'status': 'ok', 'FIREBASE_CREDENTIALS': FIREBASE_CREDENTIALS})
 
     return app
