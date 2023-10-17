b8aa109ad8a3a30a157c8c7f9425493c2e781e1a
Hector G <hmgarcia06@gmail.com>
2023-10-16 20:08:22 -0400

    added print len of private key

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 from routes import register, allowed_origins
+from config import FIREBASE_CREDENTIALS
 
+print("len(FIREBASE_CREDENTIALS)", len(FIREBASE_CREDENTIALS['private_key']))
 def create_app(config_name='config.py'):
     app = Flask(__name__)
     app.config.from_pyfile(config_name)
